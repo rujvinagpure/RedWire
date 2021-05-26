7c64d5e469622b422d7ed13a1671d204aa4310ee
Rujvi <nagpurerujvi1201@gmail.com>
2021-05-26 18:23:21 +0530

    video start

--- src/components/home/videos/index.js
+++ src/components/home/videos/index.js
@@ -1,18 +1,53 @@
-import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { View, Text , Button} from 'react-native';
-
+import { render } from 'react-dom';
+import { View, Text, Button, ScrollView, ActivityIndicator,StyleSheet } from 'react-native';
+import { Tile } from 'react-native-elements';
 
 const VideosScreen = ({navigation}) => {
+
+    const renderVideos = () => (
+        <Tile
+            imageSrc={{uri:'https://picsum.photos/200/300'}}
+            title='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+            icon={{ name:'play-circle',type:'font-awesome',color:'#fff',size:50}}
+            contentContainerStyle={styles.contentContainerStyle}
+            containerStyle={styles.containerStyle}
+            titleStyle={{fontSize:15}}
+            onPress={()=> navigation.navigate('Video_screen',{
+                id:'hsdvhsjdv',
+                postData:{}
+            })}
+        />
+    )
+
+
     return(
-        <View>
-            <Text>Videos</Text>
-            <Button
-                title ="see video"
-                onPress ={() => navigation.navigate('Video_screen')}
-            />
-        </View>
+        <ScrollView>
+            <View style={{padding:20}}>
+                {renderVideos()}
+            </View>
+            {/* <Button
+                title="see article"
+                onPress={()=> navigation.navigate('Video_screen')}
+            /> */}
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    contentContainerStyle:{
+        backgroundColor:'#fff',
+        borderWidth:1,
+        borderColor:'#e1e8ee',
+        shadowColor:'rgba(0,0,0,.2)'
+    },
+    containerStyle:{
+        width:'100%',
+        height:250,
+        marginBottom:15
+    }
+})
+
+
 
 export default VideosScreen;
